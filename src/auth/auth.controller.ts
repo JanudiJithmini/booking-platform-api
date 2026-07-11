8115e1a1031bb745254e835f59bccac8bd08c035
JanudiJithmini <akmeemanajanudi@gmail.com>
2026-07-11 11:00:45 +0530

    Clean the code in auth.contoller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,12 +6,12 @@ import { LoginDto } from './dto/login.dto';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
+  // POST /auth/register
   @Post('register')
   register(@Body() registerDto: RegisterDto) {
     return this.authService.register(registerDto);
   }
-
+  // POST /auth/login
   @Post('login')
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
